11fab9b4cc1c45dba7eaf690caf4c0f7bf507e9f
Alexander <dsiv@dsiv.local>
2026-07-11 22:13:54 +0300

    add optimization; add diagnostic threads

--- main.cpp
+++ main.cpp
@@ -18,9 +18,12 @@ void replaceLastFourBytes(std::vector<char> &data, uint32_t value) {
 }
 
 void hackEngine(std::vector<char> result, size_t idxStart, size_t idxEnd,
-  uint32_t originalCrc32, std::vector<char>& res_ret, uint32_t prev = 0xFFFFFFFF)
+  uint32_t originalCrc32, std::vector<char>& res_ret, uint32_t prev = 0xFFFFFFFF,
+  bool isPrintOut = false)
 {
+  int lastPct = -1;
   for (size_t i = idxStart; i < idxEnd; i++) {
+
     // Заменяем последние четыре байта на значение i
     replaceLastFourBytes(result, uint32_t(i));
     // Вычисляем CRC32 текущего вектора result
@@ -28,7 +31,6 @@ void hackEngine(std::vector<char> result, size_t idxStart, size_t idxEnd,
 
     if (currentCrc32 == originalCrc32) {
       std::cout << "Success\n";
-      // std::copy(result.begin(), result.end(), res_ret.begin());
       res_ret = result;
       m.lock();
       FIND_VALUE = true;
@@ -37,12 +39,60 @@ void hackEngine(std::vector<char> result, size_t idxStart, size_t idxEnd,
     }
     if (FIND_VALUE) return;
     // Отображаем прогресс
-    if (i % 1000 == 0) {
-      std::cout << "progress thread: " << std::this_thread::get_id() << " "
-                << (static_cast<double>(i - idxStart) / static_cast<double>(idxEnd - idxStart))
-                << std::endl;
+    if (isPrintOut) {
+      int pct = static_cast<int>((i - idxStart) * 100 / (idxEnd - idxStart));
+      if (pct != lastPct && pct % 10 == 0) {
+        lastPct = pct;
+        std::cout << "progress thread: " << std::this_thread::get_id() << " "
+                  << pct << " %" << std::endl;
+      }
     }
   }
+}
+
+unsigned int getDynamicNumThreads()
+{
+  const unsigned int NUM_TEST = 10000000;
+  unsigned int n = std::thread::hardware_concurrency();
+  std::vector<float> vTimeTest(n);
+  size_t idxMin = 0;
+  std::vector<char> testSequence = {'T', 'E', 'S', 'T', 'C', 'R', 'S', 'F', 'U', 'N', 'C',
+    'T', 'I', 'O', 'N'};
+  float min = std::numeric_limits<float>::max();
+  for (size_t i = 0; i < vTimeTest.size(); i++) {
+    std::vector<std::thread> vThread;
+    auto start = std::chrono::steady_clock::now();
+    std::vector<char> retRes{' '};
+    for (size_t num = 0; num < i+1; num++) {
+      vThread.emplace_back(
+        hackEngine, testSequence, static_cast<size_t>(num * NUM_TEST / (i + 1)),
+          static_cast<size_t>((num+1) * NUM_TEST / (i + 1)), 1, std::ref(retRes),
+          0xFFFFFFFF, false
+      );
+    }
+
+    for (size_t j = 0; j < vThread.size(); j++) {
+      vThread[j].join();
+    }
+    auto end = std::chrono::steady_clock::now();
+    auto timeTesting = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    vTimeTest[i] = static_cast<float>(timeTesting.count()) / static_cast<float>(NUM_TEST);
+
+    if (vTimeTest[i] < min) {
+      min = vTimeTest[i];
+      idxMin = i;
+    }
+  }
+
+  std::cout << "-------------------------------" << std::endl;
+  std::cout << "|\tTHREAD DIAGNOSTIC:\t|" << std::endl;
+  for (size_t i = 0; i < n; i++) {
+    std::cout << "| # " << i+1 << " " << vTimeTest[i] << " milliseconds\t" << "|" << std::endl;
+  }
+  std::cout << "| min time with treads = " << idxMin + 1 << "\t|" << std::endl;
+  std::cout << "-------------------------------" << std::endl;
+  return static_cast<unsigned int>(idxMin + 1);
+
 }
 
 /**
@@ -72,14 +122,15 @@ std::vector<char> hack(const std::vector<char> &original,
   const size_t maxVal = std::numeric_limits<uint32_t>::max();
   std::vector<char> ret_res{};
 
-  auto start = std::chrono::steady_clock::now();
   std::vector<std::thread> v_thread{};
-  unsigned int num_thread = std::thread::hardware_concurrency();
+  // unsigned int num_thread = std::thread::hardware_concurrency();
+  unsigned int num_thread = getDynamicNumThreads();
+  auto start = std::chrono::steady_clock::now();
   for (size_t i = 0; i < num_thread; i++) {
     v_thread.emplace_back(
       hackEngine, result, static_cast<size_t>(i * maxVal / num_thread),
         static_cast<size_t>((i+1) * maxVal / num_thread), originalCrc32, std::ref(ret_res),
-        prevCrc
+        prevCrc, true
     );
   }
 
